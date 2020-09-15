23605aa1b51bf261d43407e02d68c55e819ece92
Your Name <you@example.com>
2020-09-16 00:11:26 +0200

    Finnished challenge 47-Towers of Hanoi

--- Challenges/src/Challenge47/TowerOfHanoi.java
+++ Challenges/src/Challenge47/TowerOfHanoi.java
@@ -3,11 +3,18 @@ package Challenge47;
 public class TowerOfHanoi {
 
     public static void main(String[] args){
-     
+        towerOfHanoi(3, 'A', 'C', 'B');  // n is the amount of plates you want to move
     }
+    // Recursive function
+    static void towerOfHanoi(int n, char fromRod, char auxilaryRod, char destinationRod) {
 
+        if(n == 1){
+            System.out.println("Plate 1 was moved from " + fromRod + " to " + destinationRod);
+            return;
+        }
 
-
-
-
+        towerOfHanoi(n-1, fromRod, destinationRod,auxilaryRod);
+        System.out.println("Plate " + n + " was moved from " + fromRod + " to " + destinationRod);
+        towerOfHanoi(n-1, auxilaryRod, fromRod, destinationRod);
+    }
 }
