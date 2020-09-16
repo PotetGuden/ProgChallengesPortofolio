e46987010637c5de3cf192f268af22e594288e60
Your Name <you@example.com>
2020-09-16 23:01:02 +0200

    added challenge 59 - Caesar Cipher Cracker

---
+++ Challenges/src/Challenge59/CaesarCipherCracker.java
@@ -0,0 +1,14 @@
+package Challenge59;
+
+public class CaesarCipherCracker {
+    public static void main(String[] args) {
+
+    }
+
+    public static void CaesarCipherConverter(int n, String string){ // N = how many letters to the right in alphabet it should replace each character with.
+        char[] charsInString = string.toCharArray();
+
+       
+
+    }
+}
