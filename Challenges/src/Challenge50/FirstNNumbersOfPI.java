bf6d72f2f36dbd6e80b66339bc5a48ab425e74c8
Your Name <you@example.com>
2020-09-16 21:04:48 +0200

    added comments

--- Challenges/src/Challenge50/FirstNNumbersOfPI.java
+++ Challenges/src/Challenge50/FirstNNumbersOfPI.java
@@ -6,11 +6,11 @@ public class FirstNNumbersOfPI {
         calculateNNumbersInPI(10);
         calculateNNumbersInPI(15);
     }
-
+    // Maximum 16 digits because double only can take 16 digits
     public static void calculateNNumbersInPI(int n){
         double pi = 0;
 
-        for (int i = 100000; i > 0; i--) { // the larger (i)
+        for (int i = 100000; i > 0; i--) { // the larger (i), the closer to PI
             pi += Math.pow(-1, i + 1) / (2 * i - 1);
             if (i == 1) {
                 pi *= 4;
