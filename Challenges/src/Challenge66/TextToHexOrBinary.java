728dffbccb12d114fabeefbd22b1b3a24f7a3840
Your Name <you@example.com>
2020-09-20 16:21:39 +0200

    Added questions and scanner for the task

--- Challenges/src/Challenge66/TextToHexOrBinary.java
+++ Challenges/src/Challenge66/TextToHexOrBinary.java
@@ -1,18 +1,46 @@
 package Challenge66;
 
+import java.util.Scanner;
+
 public class TextToHexOrBinary {
     public static void main(String[] args) {
-        binHexConverter("1111 0101 0101 0011");
+
+        preQuestions();
+        //binHexConverter(input, hexOrBinary);
+        //binHexConverter("1111 0101 0101 0011");
     }
 
-    private static void binHexConverter(String string){
+    private static void preQuestions(){
+        Scanner s = new Scanner(System.in);
+        System.out.printf("Enter a binary number: ");
+        String input = s.nextLine();
+        System.out.println("Do you want the decimal or hex representation of your number? write 'd' for decimal or 'h' for hex");
+        String checkForHexOrBinary = s.nextLine();
+        //boolean hexOrBinary = true; // Hex == True, Binary == False
+        if(checkForHexOrBinary.equals("h")){
+            binHexConverter(input, true);
+        } else if( checkForHexOrBinary.equals("d")){
+            binHexConverter(input, false);
+        } else{
+            System.out.println("You have to enter a valid letter.");
+            preQuestions();
+        }
+
+
+    }
+
+    private static void binHexConverter(String string, boolean HexOrBinary){
         string = string.replaceAll("\\s+", "");  // \\s removes whitespace characters from the string, which includes " "
-        System.out.println(string);
 
         int decimal = Integer.parseInt(string,2);
         String hexStr = Integer.toString(decimal,16);
 
-        System.out.println(decimal);
-        System.out.println(hexStr);
+        if(HexOrBinary){
+            System.out.println(hexStr);
+        } else{
+            System.out.println(decimal);
+        }
+
+
     }
 }
