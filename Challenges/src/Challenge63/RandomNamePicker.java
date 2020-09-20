eec02e3904dd5c60c2b913f4dc8cace917e89442
Your Name <you@example.com>
2020-09-20 15:46:03 +0200

    Completed Random Name Picker

--- Challenges/src/Challenge63/RandomNamePicker.java
+++ Challenges/src/Challenge63/RandomNamePicker.java
@@ -4,15 +4,12 @@ import java.util.Random;
 
 public class RandomNamePicker {
     public static void main(String[] args) {
-        String[] names1 = {"Mark", "James", "Jostein", "Sofie", "Elisabeth"};
-        randomNamePicker(names1);
+        String[] names = {"Mark", "James", "Jostein", "Sofie", "Elisabeth"};
+        randomNamePicker(names);
     }
 
     private static void randomNamePicker(String[] names){
-        //System.out.println(names[(int)Math.random()*names.length]); // Printing a random name from input
-
         Random random = new Random();
         System.out.println(names[random.nextInt(names.length)]);
-
     }
 }
