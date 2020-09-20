39d7c542fc188609685b812d1205800599d31127
Your Name <you@example.com>
2020-09-20 16:01:34 +0200

    progress on bin hex converter

--- Challenges/src/Challenge66/TextToHexOrBinary.java
+++ Challenges/src/Challenge66/TextToHexOrBinary.java
@@ -2,8 +2,17 @@ package Challenge66;
 
 public class TextToHexOrBinary {
     public static void main(String[] args) {
-        
+        binHexConverter("1111 0101 0101 0011");
     }
 
+    private static void binHexConverter(String string){
+        string = string.replaceAll("\\s+", "");  // \\s removes whitespace characters from the string, which includes " "
+        System.out.println(string);
 
+        int decimal = Integer.parseInt(string,2);
+        String hexStr = Integer.toString(decimal,16);
+
+        System.out.println(decimal);
+        System.out.println(hexStr);
+    }
 }
